9e6e7734b13081f24fe3fecc8dcdbfcce5cf36bb
jaydenli <jaydenminli@gmail.com>
2025-10-07 17:59:33 -0700

    jl - updated with actual team members' names

--- src/main/java/edu/ucsb/cs156/spring/hello/Developer.java
+++ src/main/java/edu/ucsb/cs156/spring/hello/Developer.java
@@ -36,12 +36,12 @@ public class Developer {
     public static Team getTeam() {
         // TODO: Change this to your team name
         Team team = new Team("f25-15");
-        team.addMember("Alice");
-        team.addMember("Bob");
-        team.addMember("Chris G.");
-        team.addMember("Danny");
-        team.addMember("Eve");
-        team.addMember("Frances");
+        team.addMember("Guntash G.");
+        team.addMember("Andrew X.");
+        team.addMember("Jayden L.");
+        team.addMember("Kelvin F.");
+        team.addMember("Tim N.");
+        team.addMember("Akul S.");
         return team;
     }
 }
